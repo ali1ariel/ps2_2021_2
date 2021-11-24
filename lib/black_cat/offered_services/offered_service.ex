a51f9684d432a7707ea89bc52d73a470468c1f12
Alisson <aamachado@inf.ufsm.br>
2021-11-24 13:35:54 -0300

    update: offered services changeset

--- lib/black_cat/offered_services/offered_service.ex
+++ lib/black_cat/offered_services/offered_service.ex
@@ -29,7 +29,6 @@ defmodule BlackCat.OfferedServices.OfferedService do
     offered_service
     |> cast(attrs, [:name, :type])
     |> validate_required([:name, :type])
-
-    # |> cast_assoc()
+    |> cast_assoc(:time_intervals, required: true)
   end
 end

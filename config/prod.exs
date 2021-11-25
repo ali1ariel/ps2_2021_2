aad70ff1183a5b1ddae149fea92a7ad76ccb2279
Alisson <aamachado@inf.ufsm.br>
2021-11-25 01:52:39 -0300

    .

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :black_cat, BlackCatWeb.Endpoint,
-  https: [port: {:system, "PORT"}],
+  http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "ps2-rede-transformacao.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
